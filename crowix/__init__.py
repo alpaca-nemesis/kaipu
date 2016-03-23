418948272be36bdb17a9e6602f9dfbfcc65ab7de
crowix <mdzhangkailai@gmail.com>
2016-03-23 16:45:24 +0800

    500 problem solved

--- crowix/__init__.py
+++
@@ -1,5 +0,0 @@
-
-			 	<iframe width="560" height="315" src="https://www.youtube.com/embed/wyslGHOgQ_M" frameborder="0" allowfullscreen></iframe>
-			 	<iframe width="420" height="315" src="https://www.youtube.com/embed/2f1dnzJnRPw" frameborder="0" allowfullscreen></iframe>
-			 	<iframe width="420" height="315" src="https://www.youtube.com/embed/DOzhDEizR3M" frameborder="0" allowfullscreen></iframe>
-			 	<iframe width="420" height="315" src="https://www.youtube.com/embed/-jrAKmV5bRM" frameborder="0" allowfullscreen></iframe>
